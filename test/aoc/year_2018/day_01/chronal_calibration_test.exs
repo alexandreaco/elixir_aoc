4f869a1d8c25a47013b8e602cda833db8af7c694
Alex Defreitas <alex@motel.is>
2018-12-03 17:36:08 -0500

    begins day 1

---
+++ test/aoc/year_2018/day_01/chronal_calibration_test.exs
@@ -0,0 +1,28 @@
+defmodule Aoc.Year2018.Day01.ChronalCalibrationTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2018.Day01.ChronalCalibration, import: true
+
+  alias Aoc.Year2018.Day01.ChronalCalibration
+
+  describe "part_1/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 01, year: 2018
+    test "input", %{input: input} do
+      assert input |> ChronalCalibration.part_1() == input
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 01, year: 2018
+    test "input", %{input: input} do
+      assert input |> ChronalCalibration.part_2() == input
+    end
+  end
+end
