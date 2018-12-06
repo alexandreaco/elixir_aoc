56bd77781f1bdca8ea020e94c83213e251a8f51e
Alex Defreitas <alex@motel.is>
2018-12-05 21:11:32 -0500

    Begins day 2

---
+++ test/aoc/year_2018/day_02/inventory_management_system_test.exs
@@ -0,0 +1,28 @@
+defmodule Aoc.Year2018.Day02.InventoryManagementSystemTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2018.Day02.InventoryManagementSystem, import: true
+
+  alias Aoc.Year2018.Day02.InventoryManagementSystem
+
+  describe "part_1/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 02, year: 2018
+    test "input", %{input: input} do
+      assert input |> InventoryManagementSystem.part_1() == input
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 02, year: 2018
+    test "input", %{input: input} do
+      assert input |> InventoryManagementSystem.part_2() == input
+    end
+  end
+end
